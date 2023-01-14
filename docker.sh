14e6178d045fa7f0490db8997712780ae0084347
Josh Hill <josh@pyxxilated.studio>
2023-01-14 17:07:40 +1300

    feat: Seperate Compose Files

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+awk 1 docker-compose.common.yaml   \
+    docker-compose.databases.yaml  \
+    docker-compose.admin.yaml      \
+    docker-compose.dns.yaml        \
+    docker-compose.books.yaml      \
+    docker-compose.cloud.yaml      \
+    docker-compose.media.yaml      \
+    docker-compose.metrics.yaml    \
+    docker-compose.base.yaml       \
+    | docker compose -f - "$@"
