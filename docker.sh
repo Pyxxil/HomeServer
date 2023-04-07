591c915f98859b29fc0f3d20e35b0db72ac7198b
Joshua Hill <josh@pyxxilated.studio>
2023-04-07 22:02:03 +1200

    chore: Move Books to Media

--- docker.sh
+++ docker.sh
@@ -3,7 +3,6 @@ awk 1 docker-compose.common.yaml   \
     docker-compose.databases.yaml  \
     docker-compose.admin.yaml      \
     docker-compose.dns.yaml        \
-    docker-compose.books.yaml      \
     docker-compose.cloud.yaml      \
     docker-compose.media.yaml      \
     docker-compose.metrics.yaml    \
